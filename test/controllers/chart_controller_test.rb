f1e8b3e668f087adfd83614cfbea7d204e817068
m.demichele.16@aberdeen.ac.uk <demiurgo1984@gmail.com>
2017-11-15 16:59:39 +0000

    authentication users

--- test/controllers/chart_controller_test.rb
+++ test/controllers/chart_controller_test.rb
@@ -3,16 +3,19 @@ require 'test_helper'
 class ChartControllerTest < ActionDispatch::IntegrationTest
 
     def setup
+      @user = users(:michael)
       @base_title2 = "CS2012 Assignment 2: Earthquake Data App"
     end
     
     test "should get time" do
+      log_in_as(@user)
       get time_path
       assert_response :success
       assert_select "title", "#{@base_title2}"
     end
   
     test "should get feed" do
+      log_in_as(@user)
       get feed_path
       assert_response :success
       assert_select "title", "Feed | #{@base_title2}"
