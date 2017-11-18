5f9e02e1371b0b6bddb9dc67ed3486b79d023cbd
m.demichele.16@aberdeen.ac.uk <demiurgo1984@gmail.com>
2017-11-18 16:19:18 +0000

    env prod edited

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'peaceful-peak-81480.herokuapp.com'
+  host = 'https://peaceful-peak-81480.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
