b81365b7db04e59e50aafa327d2eaca0e0834d14
m.demichele.16@aberdeen.ac.uk <demiurgo1984@gmail.com>
2017-11-23 22:37:57 +0000

    hail storms stats added

--- app/controllers/hail_controller.rb
+++ app/controllers/hail_controller.rb
@@ -1,12 +1,13 @@
 class HailController < ApplicationController
   
-  def show
-     @country = params[:country]
-     if @country != nil
-      @message = "Total number of tornadoes in #{@country} in 2016 is:#{Hail.where(:st => @country).count}"
-     else
-      @message = "No State has been selected"
-     end
-  end
+   def show
+    @country = params[:country]
+    if @country != nil
+     @message = "Total number of hail storms in #{@country} in 2016 is:#{Hail.where(:st => @country).count}"
+    else
+     @message = "No State has been selected yet"
+    end
+    return @message
+   end
   
 end
