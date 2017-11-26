8d49c8a80e1c9768e476c06a77304cb37f128f91
m.demichele.16@aberdeen.ac.uk <demiurgo1984@gmail.com>
2017-11-26 19:07:27 +0000

    assets precompiled

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( charts/time.js )
 Rails.application.config.assets.precompile += %w( charts/feed.js )
+Rails.application.config.assets.precompile += %w( charts/feed.js )
+Rails.application.config.assets.precompile += %w( twitter/show.js )
 
 
 # Add additional assets to the asset load path.
